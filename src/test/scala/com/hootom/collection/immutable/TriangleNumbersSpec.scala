99c87c075130a2f8c3513e2cdc0f0634fd00cf72
Emanuel Amaral Couto <unit73e@gmail.com>
2019-01-22 02:00:21 +0000

    Add unit tests
    The singleton triangle numbers could be a corner case since the original
    graph representation should have no edges.

--- src/test/scala/com/hootom/collection/immutable/TriangleNumbersSpec.scala
+++ src/test/scala/com/hootom/collection/immutable/TriangleNumbersSpec.scala
@@ -9,6 +9,8 @@ class TriangleNumbersSpec extends FlatSpec with Matchers {
     assert((List(7, 3, 8, 2), 20) == TriangleNumbers(7, 6, 3, 6, 8, 5, 11, 2, 10, 9).minimumPath())
     assert((List(1, 1, 1, 1), 4) == TriangleNumbers(1, 6, 1, 6, 1, 5, 11, 2, 1, 9).minimumPath())
     assert((List(1, 0, 1, 0), 2) == TriangleNumbers(1, 0, 1, 6, 1, 5, 11, 2, 0, 9).minimumPath())
+    assert((List(2, 2, 2, 2), 8) == TriangleNumbers(2, 6, 2, 3, 8, 2, 11, 2, 10, 2).minimumPath())
+    assert((List(1), 1) == TriangleNumbers(1).minimumPath())
   }
 
   "An empty triangle of numbers" should "still get the first found minimal path" in {
